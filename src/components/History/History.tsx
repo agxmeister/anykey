d26ec6a7d7a02a816889e3148e2221211e3e68fd
agxmeister <agxmeister@gmail.com>
2024-12-06 00:22:56 +0200

    Add message formatting on the History tab.

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as styles from "./History.module.sass"
 import {useState} from "react";
 import {InsightDataConversationItem} from "../App/App";
+import classNames from "classnames";
 
 type BookmarkProps = {
     conversation?: InsightDataConversationItem[]
@@ -22,9 +23,14 @@ export default function History({conversation}: BookmarkProps)
                     className={styles.tab}
                     onClick={() => toggleIsOpen()}
                 >
-                    {conversation ? conversation.map((message) => (
-                        <div>
-                            <span>{message.role}</span>
+                    {conversation ? conversation.reverse().map((message) => (
+                        <div
+                            className={classNames(
+                                styles.message,
+                                message.role === "user" ? styles.user : styles.assistant,
+                            )}
+                        >
+                            <span>{message.role === "user" ? "You" : "Assistant"}</span>
                             {message.message}
                         </div>
                     )) : null}
